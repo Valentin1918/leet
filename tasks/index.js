792618aec0ae63d0ff50cd0143408f88d2880528
Grekulov <grekulov.valentyn@gmail.com>
2019-07-23 20:15:04 +0300

    insertion sort

--- tasks/index.js
+++ tasks/index.js
@@ -1,8 +1,10 @@
 // import { twoSum_1, twoSum_2 } from './twoSum';
 const { twoSum_1, twoSum_2, twoSum_3, twoSum_4, twoSum_5 } = require('./twoSum');
-const { bubbleSort, gnomeSort } = require('./sorting');
+const { bubbleSort, gnomeSort, insertionSort } = require('./sorting');
 
-const numsArr = [2, 7, 11, 15, 1, 2, 2, 3, 87, 16, 9, 10, 80, 87, 99, 11, 75, 8, 43, 33, 0, 97, 66, 7, 1, 4];
+const makeArr = n => Array.from({length: n}, () => Math.floor(Math.random() * n));
+const intArr = makeArr(25000);
+
 const target = 9;
 // console.log('twoSum_1', twoSum_1([2, 7, 11, 15], target));
 // console.log('twoSum_2', twoSum_2([2, 7, 11, 15], target));
@@ -10,5 +12,6 @@ const target = 9;
 // console.log('twoSum_4', twoSum_4([3, 2, 4], 6));
 // console.log('twoSum_5', twoSum_5([3, 2, 4], 6));
 
-console.log('bubbleSort', bubbleSort([...numsArr]));
-console.log('gnomeSort', gnomeSort([...numsArr]));
+console.log('bubbleSort', bubbleSort([...intArr]));
+console.log('gnomeSort', gnomeSort([...intArr]));
+console.log('insertionSort', insertionSort([...intArr]));
--- tasks/sorting.js
+++ tasks/sorting.js
@@ -1,6 +1,7 @@
 const change = (arr, y) => [arr[y - 1], arr[y]] = [arr[y], arr[y - 1]];
 
 const bubbleSort = arr => {
+  console.time('bubbleSort');
   let l = arr.length, i, y, counter = 0;
   for (i = 0; i < l; i++) {
     for (y = 1; y < l - i; y++) {
@@ -8,24 +9,50 @@ const bubbleSort = arr => {
       if (arr[y - 1] > arr[y]) change(arr, y);
     }
   }
+  console.timeEnd('bubbleSort');
   console.log('counter', counter);
   return arr;
 };
 
 const gnomeSort = arr => {
+  console.time('gnomeSort');
   let l = arr.length, i, y, counter = 0;
     for (y = 1; y < l; y++) {
       if (arr[y] < arr[y - 1]) {
         for (i = y; i > 0; i--) {
-          if (arr[i] >= arr[i - 1]) break;
           counter++;
+          if (arr[i] >= arr[i - 1]) break;
           change(arr, i);
         }
       }
       counter++;
     }
+  console.timeEnd('gnomeSort');
   console.log('counter', counter);
   return arr;
 };
 
-module.exports = { bubbleSort, gnomeSort };
+const insertionSort = arr => {
+  console.time('insertionSort');
+  let l = arr.length, i, y, counter = 0;
+  for (y = 1; y < l; y++) {
+    if (arr[y] < arr[y - 1]) {
+      const value = arr.splice(y, 1)[0];
+      i = y;
+      do {
+        counter++;
+        i--;
+        if (value >= arr[i] || !i) {
+          arr.splice(i + +!!i, 0, value);
+          break;
+        }
+      } while (i);
+    }
+    counter++;
+  }
+  console.timeEnd('insertionSort');
+  console.log('counter', counter);
+  return arr;
+};
+
+module.exports = { bubbleSort, gnomeSort, insertionSort };
