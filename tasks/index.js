47f78a7be28c77340700521eabbcbb9658749cec
Grekulov <grekulov.valentyn@gmail.com>
2019-09-06 15:01:37 +0300

    added singletons

--- tasks/index.js
+++ tasks/index.js
@@ -1,9 +1,10 @@
 const { twoSum_1, twoSum_2, twoSum_3, twoSum_4, twoSum_5 } = require('./twoSum');
 const { bubbleSort, gnomeSort, insertionSort } = require('./sorting');
 const { isAnagram_1, isAnagram_2, isAnagram_3, isAnagram_4 } = require('./isAnagram');
+const { Singleton_1, Singleton_2, Ext_Sin_2 } = require('./singleton');
 
-const makeArr = n => Array.from({length: n}, () => Math.floor(Math.random() * n));
-const intArr = makeArr(25000);
+// const makeArr = n => Array.from({length: n}, () => Math.floor(Math.random() * n));
+// const intArr = makeArr(25000);
 
 const target = 9;
 // console.log('twoSum_1', twoSum_1([2, 7, 11, 15], target));
@@ -16,6 +17,20 @@ const target = 9;
 // console.log('gnomeSort', gnomeSort([...intArr]));
 // console.log('insertionSort', insertionSort([...intArr]));
 
-console.log('isAnagram_1', isAnagram_4('anagram', 'nagaram'));
-console.log('isAnagram_1', isAnagram_4('car', 'cat'));
-console.log('isAnagram_1', isAnagram_4('ab', 'a'));
+// console.log('isAnagram_1', isAnagram_4('anagram', 'nagaram'));
+// console.log('isAnagram_1', isAnagram_4('car', 'cat'));
+// console.log('isAnagram_1', isAnagram_4('ab', 'a'));
+
+// const sin1 = Singleton_1.instance;
+// const sin2 = Singleton_1.instance;
+// console.log('Singleton_1', sin1 === sin2);
+
+// const sin3 = new Singleton_2('Vasia');
+// const sin4 = new Singleton_2('Petia');
+// console.log('Singleton_2', sin3.getName(), sin4.getName(), sin3 === sin4);
+
+// const sin5 = new Ext_Sin_2('Igor');
+// const sin6 = new Ext_Sin_2('Ken');
+// console.log("getName" in sin5);
+// console.log("shoutName" in sin5); // --> false --> The extension process can't even start, because SingletonClass constructor returns the instance first thing.
+// console.log('Ext_Sin_2', sin5.getName(), sin6.getName(), sin5 === sin6);
